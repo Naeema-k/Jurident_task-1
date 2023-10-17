0b7778012054c5ed1f5ca0102ffd7dbb9645cec4
Naeema-k <91788820+Naeema-k@users.noreply.github.com>
2023-10-17 23:05:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-var clickCount = 0;
-var button = document.getElementById("myButton");
+var clickCount = 0; // Declare a variable to store the click count
+var button = document.getElementById("myButton"); // Get the button element with the ID "myButton"
 
+// Add an event listener to the button for the "click" event
 button.addEventListener("click", function() {
-  clickCount++;
-  document.getElementById("clickCount").innerHTML = clickCount;
+  clickCount++; //Increment the click count variable by 1
+  document.getElementById("clickCount").innerHTML = clickCount; // Update the text of the element with the ID "clickCount" to display the new click count
 });
